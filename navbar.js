1fbdd0907719f65f32eff89f480bf7e54f7105b6
Datta0237 <dattapatil9764@gmail.com>
2022-09-28 22:57:53 +0530

    added code

---
+++ navbar.js
@@ -0,0 +1,37 @@
+function newsfeed(){
+    let newspop=document.querySelector("#popup");
+    newspop.style.display="block";
+};
+
+function newsblock(){
+    let newspop=document.querySelector("#popup");
+    newspop.style.display="none";
+}
+
+function opiniondiv(){
+    const op=document.querySelector("#opinionpop");
+    op.style.display="block";
+};
+
+function opinionblock(){
+    const op=document.querySelector("#opinionpop");
+    op.style.display="none";
+}
+function businessdiv(){
+    const busi=document.querySelector("#businesspop");
+    busi.style.display="block";
+};
+
+function businessblock(){
+    const busi=document.querySelector("#businesspop");
+    busi.style.display="none";
+}
+function sportsdiv(){
+    const spo=document.querySelector("#businesspop");
+    spo.style.display="block";
+};
+
+function sportsblock(){
+    const spo=document.querySelector("#businesspop");
+    spo.style.display="none";
+}
